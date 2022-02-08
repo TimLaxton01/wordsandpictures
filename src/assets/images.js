b2f33e258107c770c4394b55107bf1bec3cb9d73
TimLaxton01 <tlaxton@twigeducatio.com>
2022-02-08 17:17:55 +0000

    added image

---
+++ src/assets/images.js
@@ -0,0 +1,15 @@
+import akira from "./image/AkiraVol1.jpg";
+import boys from "./image/20thCenturyBoys.jpg";
+import tek from "./image/TekkonkinkreetVol1.jpg";
+import husband from "./image/ThewayofthehousehusbandVol1.jpg";
+import font from "./image/Whatthefont.jpg";
+import friday from "./image/FridayVol1.jpg";
+import concrete from "./image/ConcreteVol1.jpg";
+import cowboy from "./image/Shaolincowboyvol1.jpg";
+import saga from "./image/SagaVol1.jpg";
+import incal from "./image/Incal.jpg";
+import kill from "./image/KilltopiaVol2.jpg";
+import soup from "./image/ElectricSoup.jpg"
+
+
+export { akira, boys, tek, husband, font, friday, concrete, cowboy, saga, incal, kill, soup}
